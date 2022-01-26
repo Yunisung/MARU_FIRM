153b5c72de20d336e662ae01f82046728e88d0e1
pys890102 <pys890102@bkwinnners.com>
2022-01-26 19:51:47 +0900

    config 링크 변경

--- src/com/pgmate/firm/util/SmsGw.java
+++ src/com/pgmate/firm/util/SmsGw.java
@@ -103,7 +103,7 @@ public class SmsGw {
 	public void getConfig(String smsCh) {
     	try{
             // 프로퍼티 파일 위치 
-            String messagePropFile = "/home/MARUeuc/MARU_FIRM/conf/messageconf.properties";
+            String messagePropFile = "/home/bkwinners/MARU/MARU_FIRM/conf/messageconf.properties";
             
             // 프로퍼티 객체 생성
             Properties props = new Properties();
