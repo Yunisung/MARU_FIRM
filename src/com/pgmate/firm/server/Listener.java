83aad8a263265e43d6a033613178aac6e0b6a488
pys890102 <pys890102@bkwinners.com>
2022-11-02 17:46:36 +0900

    원복

--- src/com/pgmate/firm/server/Listener.java
+++ src/com/pgmate/firm/server/Listener.java
@@ -30,7 +30,7 @@ public class Listener {
 
 		logger.info("Connect Client IP : [{}]", clientIp);
 
-		/*if(!firm.server.monitorIp.equals("") && clientIp.indexOf(firm.server.monitorIp) > -1){
+		if(!firm.server.monitorIp.equals("") && clientIp.indexOf(firm.server.monitorIp) > -1){
 			source = FirmUtil.MONITOR;
 			socket.socketClose();
 		}
@@ -42,13 +42,13 @@ public class Listener {
 			source = FirmUtil.INTERNAL;
 		}else{
 			source = FirmUtil.KSNET;
-		}*/
+		}
 //		else {
 //			source = FirmUtil.HYPHEN;
 //		}
 
 		//PYS : BankProcess TEST
-		source = FirmUtil.KSNET;
+//		source = FirmUtil.KSNET;
 
 
 
